c43e0fdcf0e2c4e555cd89cec57949c6cc9c717f
Your Name <you@example.com>
2025-05-04 17:42:52 +0300

    11.9 fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,11 @@ module.exports = {
   'plugins': [
     'react', 'jest', 'cypress'
   ],
+  'settings': {
+    'react': {
+      'version': 'detect'
+    }
+  },
   'rules': {
     'indent': [
       'error',
